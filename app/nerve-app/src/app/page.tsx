3801282033fe7aa032d49327fbebd1668cbb9aa5
Mike Kabbabe <83938405+33Mic@users.noreply.github.com>
2025-05-25 13:22:21 -0600

    Added authentication to Nerve!
    Users can now log in and have their details logged into supabase
    Added private pages and redirecting for authenticated Users
    New users will now get a sign up email to confirm their email
    Implemented signup/login page (very barebones)
    User authentication persists through website (where required)

--- app/nerve-app/src/app/page.tsx
+++ app/nerve-app/src/app/page.tsx
@@ -1,18 +1,28 @@
 import { Button } from "@/components/ui/button";
 import Link from "next/link";
-// import { redirect } from "next/navigation";
+// import { logout } from "./logout/actions";
+import { createClient } from "@/utils/supabase/server";
+import { redirect } from "next/navigation";
 
-export default function Home() {
-  // redirect("challenge/");
+export default async function Home() {
+  // If logged in, redirect to challenge page
+  const supabase = await createClient();
+
+  const { data, error } = await supabase.auth.getUser();
+  if (!(error || !data?.user)) {
+    redirect('/challenge');
+  }
+
+  // Otherwise give option to go to login page
   return (
     <div className="flex flex-col justify-center items-center h-screen">
       <main>
         <h1 className="text-4xl">Welcome to Nerve v1.0!</h1>
         {/* <Link href="login/"> */}
-        <Link href="challenge/">
-
+        <Link href="login/">
           <Button className="font-bold">Sign up!</Button>
         </Link>
+        {/* <Button className="font-bold" onClick={logout}> Log Out </Button> */}
       </main>
     </div>
   );
